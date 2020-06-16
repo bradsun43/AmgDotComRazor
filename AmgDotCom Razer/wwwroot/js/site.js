0c1592f6554f226cfdbc0de41d8de96df0dc9961
Brad Sunshine <39799222+bradsun43@users.noreply.github.com>
2020-06-16 10:52:42 -0400

    AMG Dot COME

---
+++ AmgDotCom Razer/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+})
+
+$('.dropdown-toggle').dropdown()
+
+
+$(document).ready(function () {
+    $('.count').prop('disabled', true);
+    $(document).on('click', '.plus', function () {
+        $('.count').val(parseInt($('.count').val()) + 1);
+    });
+    $(document).on('click', '.minus', function () {
+        $('.count').val(parseInt($('.count').val()) - 1);
+        if ($('.count').val() == 0) {
+        $('.count').val(0);
+        }
+    });
+});
+
