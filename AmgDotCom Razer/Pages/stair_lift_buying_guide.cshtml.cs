96e3ec50801362c75543d057701d8986ff5b2a63
Brad Sunshine <39799222+bradsun43@users.noreply.github.com>
2020-07-09 09:47:55 -0400

    updates

---
+++ AmgDotCom Razer/Pages/stair_lift_buying_guide.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AmgDotCom_Razer.Pages
+{
+    public class stair_lift_buying_guideModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
